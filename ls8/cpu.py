0e2a6ff2a97c34f133b51aa3667061cb3efce691
fluxlog <fluxlog@hotmail.com>
2020-08-21 09:27:23 -0700

    sprint arch

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,56 +1,133 @@
 """CPU functionality."""
-
 import sys
-
-LDI = 0b10000010
-HLT = 0b00000001
-PRN = 0b01000111
-MUL = 0b10100010
-POP = 0b01000110
-PUSH = 0b01000101
 
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256
-        self.reg = [0] * 8
-        self.pc = 0
         self.running = True
-        self.sp = len(self.reg) - 1
+        self.ram = [0] * 256        # Memory
+        self.registers = [0] * 8    # Numeric Registers R0-R7
+        self.pc = 0                 # Counter
+        self.sp = 7
+        self.fl = 0b00000000
 
-    def ram_read(self, value):
-        return self.ram[value]
+        self.table = {
+            0b00000001: self.HLT,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b10100000: self.ADD,
+            0b10100010: self.MUL,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET,
+            0b10100111: self.CMP,
+            0b01010100: self.JMP,
+            0b01010101: self.JEQ,
+            0b01010110: self.JNE
+        }
+    
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+    
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
+    
+    def HLT(self, a, b):
+        self.running = False
 
-    def ram_write(self, value, register):
-        self.ram[value] = register
+    def LDI(self, a, b):
+        self.registers[a] = b
+        self.pc += 3
 
+    def PRN(self, a, b):
+        print(self.registers[a])
+        self.pc += 2
+    
+    def ADD(self, a, b):
+        self.alu("ADD", a, b)
+        self.pc += 3
 
-    def load(self):
+    def MUL(self, a, b):
+        self.alu("MUL", a, b)
+        self.pc += 3
+    
+    def PUSH(self, a, b):
+        self.sp -= 1
+        self.ram[self.sp] = self.registers[a]
+        self.pc += 2
+
+    def POP(self, a, b):
+        self.registers[a] = self.ram[self.sp]
+        self.ram[self.sp] = 0
+        self.sp += 1
+        self.pc += 2
+    
+    def CALL(self, a, b):
+        return_address = self.pc + 2
+        self.registers[self.sp] -= 1
+        self.ram[self.registers[self.sp]] = return_address
+        self.pc = self.registers[a]
+
+    def RET(self, a, b):
+        return_address = self.ram[self.registers[self.sp]]
+        self.registers[self.sp] += 1
+        self.pc = return_address
+
+    def CMP(self, a, b):
+        self.fl = 0b00000000
+        self.alu('CMP', a, b)
+        self.pc += 3
+
+    def JMP(self, a, b):
+        address = self.registers[a]
+        self.pc = address
+
+    def JEQ(self, a, b):
+        address = self.registers[a]
+        
+        if self.fl == 0b00000001:
+            self.pc = address
+        else:
+            self.pc += 2
+
+    def JNE(self, a, b):
+        address = self.registers[a]
+
+        if self.fl != 0b00000001:
+            self.pc = address
+        else:
+            self.pc += 2
+
+    def load(self, file):
         """Load a program into memory."""
+        address = 0
 
-        filename = sys.argv[1]
-
-
-        with open(filename) as files:
-            for address, line in enumerate(files):
-                line = line.split("#")
-                try:
-                    v = int(line[0], 2)
-                except ValueError:
+        with open(file) as f:
+            for line in f:
+                if line[0] == '\n' or line[0] == '#':
                     continue
-                self.ram_write(address, v)
 
+                binary_code = line[:8]
+                ram_value = int(binary_code, 2)
+
+                self.ram[address] = ram_value
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-            # added the option for multiplication
+            self.registers[reg_a] += self.registers[reg_b]
         elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
+            self.registers[reg_a] *= self.registers[reg_b]
+        elif op == "CMP":
+            if self.registers[reg_a] < self.registers[reg_b]:
+                self.fl = 0b00000100
+            elif self.registers[reg_a] > self.registers[reg_b]:
+                self.fl = 0b00000010
+            elif self.registers[reg_a] == self.registers[reg_b]:
+                self.fl = 0b00000001
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -59,7 +136,6 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -70,66 +146,15 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.registers[i], end='')
 
         print()
 
-    def LDI(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-
-        self.reg[operand_a] = operand_b
-
-        self.pc += 3
-
-    def PRN(self):
-        operand_a = self.ram_read(self.pc + 1)
-        print(self.reg[operand_a])
-
-        self.pc += 2
-
-    def HLT(self):
-        self.running = False
-    
-    def MUL(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-
-        self.alu("MUL", operand_a, operand_b)
-
-        self.pc += 3
-
-    def POP(self):
-        self.reg[self.ram[self.pc + 1]] = self.reg[self.sp]
-        self.sp += 1
-        self.pc += 2
-
-    def PUSH(self):
-        self.sp -= 1
-        self.reg[self.sp] = self.reg[self.ram[self.pc + 1]]
-        self.pc += 2
-
-    def call_function(self, n):
-        branch_table = {
-            LDI : self.LDI,
-            PRN : self.PRN,
-            HLT : self.HLT,
-            MUL : self.MUL,
-            POP : self.POP,
-            PUSH : self.PUSH
-        }
-
-        files = branch_table[n]
-
-        if branch_table.get(n) is not None:
-            files()
-        else:
-            print(f"Unknown instruction {n}")
-            sys.exit(1)
-
     def run(self):
         """Run the CPU."""
-        # While true
         while self.running:
-            IR = self.ram_read(self.pc)
-            self.call_function(IR)
+            IR = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            self.table[IR](operand_a, operand_b)
